1a76ec942719507e18c3d444412ce853e61bd40b
Komal Sharma <ks469828@gmail.com>
2024-03-08 23:56:31 -0400

    operater

---
+++ operator.js
@@ -0,0 +1,81 @@
+// Arithmetic Operators
+let num1 = 22;
+let num2 = 22;
+// let resultAddition = num1 + num2; // Addition
+// let resultSubtraction = num1 - num2; // Subtraction
+// let resultMultiplication = num1 * num2; // Multiplication
+// let resultDivision = num1 / num2; // Division
+// let resultModulus = num1 % num2; // Modulus
+// let incrementValue = 8;
+// incrementValue++; // Increment
+// let decrementValue = 10;
+// decrementValue--; // Decrement
+
+// console.log("num1+num2=", num1+num2); // Output: Addition: 15
+// console.log("Subtraction:", resultSubtraction); // Output: Subtraction: 5
+// console.log("Multiplication:", resultMultiplication); // Output: Multiplication: 50
+// console.log("Division:", resultDivision); // Output: Division: 2
+// console.log("Modulus:", resultModulus); // Output: Modulus: 0
+// console.log("Increment:", incrementValue); // Output: Increment: 6
+// console.log("Decrement:", decrementValue); // Output: Decrement: 9
+
+// Comparison Operators
+// let isEqual = (num1 == num2); // Equal
+// let isNotEqual = (num1 != num2); // Not Equal
+// let isStrictEqual = (num1 === num2); // value and data type check(9==9)
+// let isStrictNotEqual = (num1 !== num2); // value and data type both will check(9!==9)
+// let isGreaterThan = (num1 > num2); // Greater Than
+// let isLessThan = (num1 < num2); // Less Than
+// let isGreaterOrEqual = (num1 >= num2); // Greater Than or Equal To
+// let isLessOrEqual = (num1 <= num2); // Less Than or Equal To
+
+// console.log("Equal:", isEqual); // Output: Equal: false
+// console.log("Not Equal:", isNotEqual); // Output: Not Equal: true
+// console.log("Strict Equal:", isStrictEqual); // Output: Strict Equal: false
+// console.log("Strict Not Equal:", isStrictNotEqual); // Output: Strict Not Equal: true
+// console.log("Greater Than:", isGreaterThan); // Output: Greater Than: true
+// console.log("Less Than:", isLessThan); // Output: Less Than: false
+// console.log("Greater Than or Equal To:", isGreaterOrEqual); // Output: Greater Than or Equal To: true
+// console.log("Less Than or Equal To:", isLessOrEqual); // Output: Less Than or Equal To: false
+
+// Logical Operators
+let condition1 = true;
+let condition2 = false;
+// let andResult = condition1 && condition2; // AND
+// let orResult = condition1 || condition2; // OR
+// let notResult = !condition1; // NOT(it will make o/p of the value for only first conditon 0 to 1 and vice versa)
+
+// console.log("AND:", andResult); // Output: AND: false
+// console.log("OR:", orResult); // Output: OR: true
+// console.log("NOT:", notResult); // Output: NOT: false
+
+// Assignment Operators( it is used to perform mathamatical operations and assign or update the value of the variables)
+// let x = 5;
+// x += 3; // Addition Assignment x=x+ 3
+// console.log("Addition Assignment:", x); // Output: Addition Assignment: 8
+// let y = 10;
+// y -= 4; // Subtraction Assignment
+// console.log("Subtraction Assignment:", y); // Output: Subtraction Assignment: 6
+
+// let z = 7;
+// z *= 2; // Multiplication Assignment
+// console.log("Multiplication Assignment:", z); // Output: Multiplication Assignment: 14
+
+// let w = 12;
+// w /= 3; // Division Assignment
+// console.log("Division Assignment:", w); // Output: Division Assignment: 4
+
+// Unary Operators
+let operand = 10;
+let unaryPlus = +operand; // Unary Plus
+let unaryNegation = -operand; // Unary Negation
+let logicalNot = !condition1; // Logical Not
+let incrementUnary = ++operand; // Increment
+let decrementUnary = --operand; // Decrement
+// unary opeteror(when only single operator is used)
+
+console.log("Unary Plus:", unaryPlus); // Output: Unary Plus: 10
+console.log("Unary Negation:", unaryNegation); // Output: Unary Negation: -10
+console.log("Logical Not:", logicalNot); // Output: Logical Not: false
+console.log("Increment Unary:", incrementUnary); // Output: Increment Unary: 11
+console.log("Decrement Unary:", decrementUnary); // Output: Decrement Unary: 10
---
+++ timesystem.js
@@ -0,0 +1,86 @@
+// let a = new Date().getHours();/*it will calculate only time*/
+// console.log(a);
+// if (a>=8 &&a<12){console.log("good morning")}
+// else if (a>=12 &&a<16){console.log("good afternoon")
+// }
+// else if (a>=16 &&a<21){console.log("good evening")
+// }
+// else{console.log("good night")};
+
+
+// if else
+// let age=7;
+// if (age>7)
+//     {console.log("yor are right")}
+// else
+//     {console.log("you are wrong")}
+
+// Prompt the user to enter a character
+// let character = "a"; // For example, let's say the user enters 'a'
+
+
+// // Check if the character is a vowel using a switch statement
+// switch (character) {
+//   case 'a':
+//     console.log("The entered character 'a' is a vowel.");
+//     break;
+//   case 'e':
+//     console.log("The entered character 'e' is a vowel.");
+//     break;
+//   case 'i':
+//     console.log("The entered character 'i' is a vowel.");
+//     break;""
+//   case 'o':
+//     console.log("The entered character 'o' is a vowel.");
+//     break;
+//   case 'u':
+//     console.log("The entered character 'u' is a vowel.");
+//     break;
+//   default:
+//     console.log("The entered character is not a vowel.");
+// }
+
+let a ="mmm";
+switch(a){
+case 'jan':
+  console.log("the month is january");
+  break;
+  case'feb':
+  console.log("the month is february");
+  break;
+  case'march':
+  console.log("the month is march");
+  break;
+  case'aprail':
+  console.log("the month is april");
+  break;
+  case'may':
+  console.log("the month is may");
+  break;
+  case 'june':
+  console.log("the month is june");
+  break;
+  case 'july':
+  console.log("the month is july");
+  break;
+  case 'aug':
+  console.log("the month is aug");
+  break;
+  case 'sep':
+    console.log("the month is sep");
+  break;
+  case 'oct':
+  console.log("the month is oct");
+  break;
+  case 'nov':
+  console.log("the month is nov");
+  break;
+  case 'dec':
+  console.log("the month is dec");
+  break;
+  default:console.log("The entered character is not a month.");
+  }
+
+
+    
+  
